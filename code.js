7e4f2ee128961ba33d2618bb7a7a747345823d07
kaankarabiber <kaankarabiber1@gmail.com>
2023-08-30 19:45:37 +0300

    add functionality to change dimensions button

--- code.js
+++ code.js
@@ -4,11 +4,16 @@ function createDivELement(){
             const newDiv = document.createElement('div');
             newDiv.classList.add('cells');
             let row = document.querySelector(`.row${j + 1}`);
-            console.log(row);
             row.appendChild(newDiv);
         }
     }
 }
+let changeDimensionsButton = document.querySelector('button');
+changeDimensionsButton.addEventListener('click',() => {
+    let answer = prompt('type dimension');
+    console.log(answer);
+    
+})
 createDivELement();
 
 
