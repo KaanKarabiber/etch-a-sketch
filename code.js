066badbd8c0b6bc0fea5373589a966a18e63ab7c
kaankarabiber <kaankarabiber1@gmail.com>
2023-08-31 15:33:14 +0300

    modified createGrid function to set width and height of cells in creation rather then setting it on the css file

--- code.js
+++ code.js
@@ -1,13 +1,16 @@
-function createGrid(){
+function CreateGrid(dimension){
+    widthHeightValue = (100 / dimension) + '%';
     const container = document.createElement('div');
     container.classList.add('container');
     document.body.append(container);
-    for(let i = 0; i < 16; i++){
+    for(let i = 0; i < dimension; i++){
         const newRow = document.createElement('div');
         newRow.classList.add('row');
-        for(let j = 0; j < 16; j++){
+        newRow.style.minHeight = widthHeightValue;
+        for(let j = 0; j < dimension; j++){
             const newDiv = document.createElement('div');
             newDiv.classList.add('cells');
+            newDiv.style.width = widthHeightValue;
             newRow.appendChild(newDiv);
         }
         container.appendChild(newRow);
@@ -21,9 +24,12 @@ let changeDimensionsButton = document.querySelector('button');
 changeDimensionsButton.addEventListener('click',() => {
     let answer = prompt('type dimension');
     console.log(answer);
+    removeGrid();
+    CreateGrid(answer);
 })
 
-createGrid();
+
+CreateGrid(16);
 
 
 
